3e370d065db5cc549eaf74ac5bc7d69ef0d7e238
vinhpna1c <vinhpna@1cinnovation.com>
2023-10-30 17:01:18 +0700

    temp

--- database/models/event_log.py
+++ database/models/event_log.py
@@ -6,11 +6,13 @@ class EventLogModel(models.Model):
     # Fields
     id = fields.IntField(pk=True)
     video_url = fields.CharField(max_length=255)
+    image_id=fields.CharField(max_length=255)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
     # relationship
     event = fields.ForeignKeyField("models.EventModel", related_name="log_event")
+    face=fields.ForeignKeyField('models.RegisteredFaceModel',related_name='face_detected')
 
     class Meta:
         table = "EventLog"
