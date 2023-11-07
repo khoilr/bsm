9590fd108caa5a4d9715c3d1f51ba57d711d8c32
wiaqen0 <wia.wia.164@gmail.com>
2023-11-07 15:24:17 +0700

    fix to_json method

--- database/models/user.py
+++ database/models/user.py
@@ -25,9 +25,11 @@ class UserModel(models.Model):
         for field_name, field_object in self._meta.fields_map.items():
             value = getattr(self, field_name)
             if isinstance(field_object, (fields.ForeignKeyField.__class__, fields.OneToOneField.__class__)):
-                value = value.person_id if value else None
-            elif isinstance(value, datetime.datetime):  
+                value = value.id if value else None
+            elif isinstance(value, datetime.datetime):
                 value = int(round(value.timestamp())) if value else None
+            elif isinstance(value,(fields.ReverseRelation,_NoneAwaitable)):
+                continue
             model_data[field_name] = value
         return {key: value for key, value in model_data.items() if not isinstance(value, QuerySet)}
   
